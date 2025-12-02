781573647954f204128a933b4c7b86b4afb5b396
Dantekkj <leonardomunhozlopes@gmail.com>
2025-12-02 00:52:28 -0300

    Delete LojaVirtual.api/LojaVirtual.api directory

--- LojaVirtual.api/LojaVirtual.api/LojaVirtual.Business/DTOs/ComputadorFiltroDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace LojaVirtual.Business.DTOs;
-
-public class ComputadorFiltroDTO
-{
-	public string? Marca { get; set; }
-	public string? Processador { get; set; }
-}
--- LojaVirtual.api/LojaVirtual.api/LojaVirtual.Business/Entities/Carrinho.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LojaVirtual.API.Entities;
-
-public class Carrinho
-{
-	public int Id { get; set; }
-	public virtual List<ProdutoItem> Itens { get; set; }
-    public decimal ValorTotal { get; set; }
-}
